606e9974e68d355f4ec008af7ecc48733358b75d
Tim Peeters <timpeeters@users.noreply.github.com>
2022-01-25 14:45:22 +0100

    Cleanup.

--- src/main/java/be/xplore/kdg/compiler/Deprecated.java
+++ src/main/java/be/xplore/kdg/compiler/Deprecated.java
@@ -6,11 +6,11 @@ class Deprecated {
 
     /**
      * Deprecated as of JDK version 1.1 (1997).
-     *
+     * <p>
      * Confusing constructor params:
-     * year – the year minus 1900.
-     * month – the month between 0-11.
-     * date – the day of the month between 1-31.
+     * year (the year minus 1900).
+     * month (the month between 0-11).
+     * date (the day of the month between 1-31).
      */
     void newDate() {
         new Date(122, 0, 26);
